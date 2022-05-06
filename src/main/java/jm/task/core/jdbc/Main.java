1122e37b121682b34e7a384502e26745d09e9235
Ильфат <mukhametovilfat@gmail.com>
2022-05-07 00:59:59 +0500

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Петя","Кузнецов",(byte) 18);
+        userService.saveUser("Иван","Иванов",(byte) 20);
+        userService.saveUser("Анна","Ивановна",(byte) 24);
+        userService.saveUser("Алюша","Папович",(byte) 35);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
-    // реализуйте настройку соеденения с БД
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class Util {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/MySQL";
+    private static final String USER = "root";
+    private static final String PASSWORD = "xkp7mj#E";
+    private static Connection connection =null;
+    private Util() {
+    }
+
+    public static Connection getConnection() {
+        try { connection= DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return connection;
+    }
 }
+
